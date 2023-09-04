cca2a2373a5bb221c9c9dd637d40da4e57c2d1cd
Trung Anh <vutrunganh1@gmail.com>
2023-09-04 21:25:53 +0700

    Add module oauth2

---
+++ Oauth-resource-server/Oauth-resource-server/src/test/java/anhvt/com/Oauth/resource/server/OauthResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package anhvt.com.Oauth.resource.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
